3ff8281265d20f99e737ff7ab02bead322a3a4a2
Wayra Loayza <wayra.loayza@fundacion-jala.org>
2021-11-18 11:22:20 -0400

    Add index changes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 8080;
 
 app.get('/', (req: any, res: any) => {
-  res.send('Hello World....')
-});
+  res.send(
+    'Hello Wjhgjjjjjjjjjjjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhorld....'
+  );
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
